b6f5e0d38da8007888bd20277dfa80adc3ec2de5
pauxiel <paul.scholes50@yahoo.com>
2021-01-06 17:07:55 -0800

    hello world oo

--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-         Fastfeedback
+         Fastfeedbacks
         </h1>
 
         <p className={styles.description}>
